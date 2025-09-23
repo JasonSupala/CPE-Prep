a94ebd3addc477ad04583b7acef5f68ffc3203b4
JasonSupala <supala.jason@gmail.com>
2025-09-23 20:28:35 +0800

    Added Odd Sum

---
+++ CPE_OneStar/19_OddSum.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int a, b;
+        cin >> a;
+        cin >> b;
+        if(a % 2 == 0){
+            a += 1;
+        }
+        if(b % 2 == 0){
+            b -= 1;
+        }
+        int sum = 0;
+        for(int j = a; j <= b; j+=2){
+            sum += j;
+        }
+        cout << "Case " << i+1 << ": " << sum << endl;
+    }
+
+    return 0;
+}
